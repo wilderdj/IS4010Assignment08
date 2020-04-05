a75c42f13788feee90b1b13ed4bb302edf7f4ded
wilde <wilde@192.168.86.45>
2020-04-05 18:22:55 -0400

    Changed pie chart to show Top 5 transactions by store.

--- Assignment08/Src/Assignment08.py
+++ Assignment08/Src/Assignment08.py
@@ -3,13 +3,19 @@ Created on Mar 20, 2020
 Assignment 08 Donor Code
 Adapted from https://openpyxl.readthedocs.io/en/stable/charts/pie.html
 @author: nicomp
+Name: Jack Wilder
+email: wilderdj@mail.uc.edu
+Assignment: Assignment 08
+Course: IS 4010
+Semester/Year: Spring 2020
+Brief Description: Editing an excel chart from eclipse.
 '''
 from openpyxl import load_workbook
 from openpyxl.chart import (
     PieChart,
     ProjectedPieChart,
     Reference
-)
+) 
 from openpyxl.chart.series import DataPoint
 from openpyxl.chart.label import DataLabelList 
 
@@ -17,11 +23,11 @@ wb = load_workbook(filename = 'Top5TransactionsByLoyaltyNumber.xlsx')
 ws = wb['Sheet1']
 
 pie = PieChart()
-labels = Reference(ws, min_col=6, min_row=2, max_row=6)
+labels = Reference(ws, min_col=4, min_row=2, max_row=6)
 data = Reference(ws, min_col=2, min_row=1, max_row=6)
-pie.add_data(data, titles_from_data=False)
+pie.add_data(data, titles_from_data=True)
 pie.set_categories(labels)
-pie.title = "Top 5 Total Transactions by Loyalty Number"
+pie.title = "Top 5 Total Transactions by Store"
 pie.dataLabels = DataLabelList()
 pie.dataLabels.showVal = True
 
